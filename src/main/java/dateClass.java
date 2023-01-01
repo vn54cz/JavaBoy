1ae646366a3c93d2667c5e2d3d0d345618969972
Binesh <krishnanbineesh20@gmail.com>
2023-01-01 16:32:06 +0530

    Fourth Push

--- src/main/java/dateClass.java
+++ src/main/java/dateClass.java
@@ -1,5 +1,6 @@
 package src.main.java;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class dateClass {
@@ -38,5 +39,22 @@ public class dateClass {
         System.out.println("Returning "+c);
 
         // For more details visit https://docs.oracle.com/javase/7/docs/api/java/util/Date.html
+
+        //Formatting dates in a definite format this is handled by SimpleDateFormat class of Java
+        // For more details on SDF visit https://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html#:~:text=SimpleDateFormat%20is%20a%20concrete%20class,patterns%20for%20date%2Dtime%20formatting.
+        // Use letter as per mentioned in the webpage to avoid garbage value
+        SimpleDateFormat sdf, sdf1, sdf2, sdf3, sdf4, sdf5;
+                sdf = new SimpleDateFormat("MM/dd/yyyy");
+                sdf1 = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+                sdf2 = new SimpleDateFormat("G");
+                sdf3 = new SimpleDateFormat("y");
+                sdf4 = new SimpleDateFormat("Y");
+                sdf5 = new SimpleDateFormat("z:Z");
+        System.out.println("Formatting date "+sdf.format(date));
+        System.out.println("Formatting date with time "+sdf1.format(date));
+        System.out.println("Era "+sdf2.format(01/01/1960));
+        System.out.println("Year "+sdf3.format(date));
+        System.out.println("Week year "+sdf4.format(date));
+        System.out.println("Time Zone "+sdf5.format(date));
     }
 }
